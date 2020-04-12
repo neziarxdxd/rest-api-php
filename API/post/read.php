2ae10255b54f99217126c24dd3d5bbeca69d4727
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2020-04-12 13:44:46 +0800

    created front

---
+++ API/post/read.php
@@ -0,0 +1,12 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+include_once('../../config/Database.php');
+
+
+
+include_once('../../models/Post.php');
+
+
+
+?>
--- models/Post.php
+++ models/Post.php
@@ -16,7 +16,15 @@ class Post{
 
     }
     public function read(){
-        $query = 'SELECT c.name as category_name,p.id, p.category,p.title, p.body,p.author,p.created';
+        $query = 'SELECT c.name as category_name,p.id, p.category,p.title, 
+        p.body,p.author,p.created FROM' .$this->table.'p LEFT JOIN categories c ON p.category_id = c.id 
+        ORDER BY p.created_at DESC';
+        
+        $statement = $this->conn->prepare($query);
+
+        $statement->execute();
+
+        return $statement;
     }
-}
+    }
 ?>
